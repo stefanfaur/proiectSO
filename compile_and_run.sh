e542a7ffdbd4978fbe3fbe335b8f50002673d2b6
stefanfaur <stefan.faur@student.upt.ro>
2023-11-07 16:32:51 +0200

    Improved compile&run script.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Compile metadata.c
-gcc -o prog metadata.c
+# Compile metadata.c with -Wall option
+gcc -Wall -o prog metadata.c
+
+# Check for compilation errors
+if [ $? -ne 0 ]; then
+    echo "Compilation failed. Exiting..."
+    exit 1
+fi
 
 # Call the compiled file with "imagine.bmp" argument
 ./prog imagine.bmp
+
